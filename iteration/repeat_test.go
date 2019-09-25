70b02dbbea42d10b294ccd72b0e2f410870ec885
rb.boley <rb.boley@aspencapital.com>
2019-09-25 09:41:39 -0700

    Refactored iteration, and added slices with a refactor to range.

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,12 +1,37 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	assertCorrectSequence := func(t *testing.T, expected, repeated string) {
+		t.Helper()
+		if expected != repeated {
+			t.Errorf("expected %q repeated %q", expected, repeated)
+		}
+	}
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
+	t.Run("Repeats five times when specified", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+		assertCorrectSequence(t, expected, repeated)
+	})
+
+	t.Run("Can pass in a count to repeat", func(t *testing.T) {
+		repeated := Repeat("a", 3)
+		expected := "aaa"
+		assertCorrectSequence(t, expected, repeated)
+	})
+}
+func ExampleRepeat() {
+	repeated := Repeat("z", 3)
+	fmt.Println(repeated)
+	// Output: zzz
+}
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
 	}
 }
